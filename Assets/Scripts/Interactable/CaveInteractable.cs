8c3ea19ba57358c4ddefc12fa0fc4f8aafc1bc16
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-04-12 18:44:29 +0300

    Make cave entrance

---
+++ Assets/Scripts/Interactable/CaveInteractable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Interactable
+{
+    public class CaveInteractable : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private string promptMessage;
+        
+        public void Interact()
+        {
+            
+        }
+
+        public string GetPromptMessage() => promptMessage;
+    }
+}
