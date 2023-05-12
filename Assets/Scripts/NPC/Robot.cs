aff65e67d3e3b1af3aba07e91a2b9bea146ad05a
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-05-12 14:53:39 +0300

    Add Paperman NPC with some dialogue options

---
+++ Assets/Scripts/NPC/Robot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace NPC
+{
+    public class Robot : NPC
+    {
+        private static readonly int waveHand = Animator.StringToHash("WaveHand");
+
+        [Tooltip("Transform at which Robot looks after finishing conversation with the player")]
+        [SerializeField] private Transform lookAt;
+
+        [Header("Robot elements")]
+        [SerializeField] private Transform headTransform;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+        
+        public override void StartConversation()
+        {
+            base.StartConversation();
+            
+            TurnTransform(headTransform, playerScriptableObject.GetActualPlayerPosition());
+            WaveHand();
+        }
+
+        public void OnEndConversation()
+            => TurnTransform(headTransform, lookAt.position);
+
+        private void WaveHand()
+            => TriggerAnimation(waveHand);
+    }
+}
